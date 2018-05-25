bd6e02d3c1455be8319bef1470dea28f4c3a43db
Shoaib Ahmed <12bscsssiddiqui@seecs.edu.pk>
2018-05-25 05:43:51 +0200

    Added model specific snapshots

--- cnn_runner.py
+++ cnn_runner.py
@@ -28,15 +28,13 @@ def get_model(model_name):
     else:
         print("Error: Model not found!")
         exit(-1)
-    # if use_gpu:
-    #     model.cuda()
     print('done')
     return model
 
 
 def predict(model_name):
     model = get_model(model_name)
-    model.load_state_dict(torch.load('best_val_weight.pth'))
+    model.load_state_dict(torch.load('best_val_weight_' + model_name + '.pth'))
     model.eval()
 
     tta_preprocess = [preprocess, preprocess_hflip]
@@ -54,7 +52,7 @@ def predict(model_name):
         'lx': lx.cpu(),
         'px': px.cpu(),
     }
-    torch.save(data, 'test_prediction.pth')
+    torch.save(data, 'test_prediction_' + model_name + '.pth')
 
     data_loaders = []
     for transform in tta_preprocess:
@@ -69,7 +67,7 @@ def predict(model_name):
         'lx': lx.cpu(),
         'px': px.cpu(),
     }
-    torch.save(data, 'val_prediction.pth')
+    torch.save(data, 'val_prediction_' + model_name + '.pth')
 
 
 def train(model_name):
@@ -94,9 +92,8 @@ def train(model_name):
         model = nn.DataParallel(model)
     model.to(device)
 
-    lr = 0.001
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-5)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2)
 
     if model_name.endswith("_focal"):
         print ("Using Focal loss instead of normal cross-entropy")
@@ -110,7 +107,8 @@ def train(model_name):
         print('Epoch: %d' % epoch)
         
         running_loss = RunningMean()
-        running_score = RunningMean()
+        running_error = RunningMean()
+        running_accuracy = RunningMean()
 
         model.train()
         pbar = tqdm(training_data_loader, total=len(training_data_loader))
@@ -130,13 +128,14 @@ def train(model_name):
 
             loss = criterion(outputs, labels)
             running_loss.update(loss.data[0], 1)
-            running_score.update(torch.sum(preds != labels.data), batch_size)
+            running_error.update(torch.sum(preds != labels.data), batch_size)
+            running_accuracy.update(torch.sum(preds == labels.data), batch_size)
 
             loss.backward()
             optimizer.step()
 
-            pbar.set_description('%.5f %.3f' % (running_loss.value, running_score.value))
-        print('Epoch: %d | Running loss: %.5f | Running score: %.3f' % (epoch, running_loss.value, running_score.value))
+            pbar.set_description('%.5f %.3f %.3f' % (running_loss.value, running_accuracy.value, running_error.value))
+        print('Epoch: %d | Running loss: %.5f | Running accuracy: %.3f | Running error: %.3f' % (epoch, running_loss.value, running_accuracy.value, running_error.value))
 
         lx, px = utils.predict(model, validation_data_loader)
         log_loss = criterion(Variable(px), Variable(lx))
@@ -148,7 +147,7 @@ def train(model_name):
         scheduler.step(log_loss)
 
         if log_loss < min_loss:
-            torch.save(model.state_dict(), 'best_val_weight.pth')
+            torch.save(model.state_dict(), 'best_val_weight_' + model_name + '.pth')
             print('Validation score improved from %.5f to %.5f. Saved!' % (min_loss, log_loss))
             min_loss = log_loss
             patience = 0
--- submit.py
+++ submit.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
@@ -5,16 +7,25 @@ import numpy as np
 import pandas as pd
 from misc import FurnitureDataset, preprocess
 
-test_dataset = FurnitureDataset('test', transform=preprocess)
+def generate_final_predictions(model_name):
+	test_dataset = FurnitureDataset('test', transform=preprocess)
 
-test_pred = torch.load('test_prediction.pth')
-test_prob = F.softmax(Variable(test_pred['px']), dim=1).data.numpy()
-test_prob = test_prob.mean(axis=2)
+	test_pred = torch.load('test_prediction_' + model_name + '.pth')
+	test_prob = F.softmax(Variable(test_pred['px']), dim=1).data.numpy()
+	test_prob = test_prob.mean(axis=2)
 
-test_predicted = np.argmax(test_prob, axis=1)
-test_predicted += 1
-result = test_predicted
+	test_predicted = np.argmax(test_prob, axis=1)
+	test_predicted += 1
+	result = test_predicted
 
-sx = pd.read_csv('data/sample_submission_randomlabel.csv')
-sx.loc[sx.id.isin(test_dataset.data.image_id), 'predicted'] = result
-sx.to_csv('sx.csv', index=False)
+	sx = pd.read_csv('data/sample_submission_randomlabel.csv')
+	sx.loc[sx.id.isin(test_dataset.data.image_id), 'predicted'] = result
+	sx.to_csv('sx_' + model_name + '.csv', index=False)
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+    parser.add_argument('model', choices=['densenet', 'squeezenet', 'resnet', 'squeezenet_focal'])
+    args = parser.parse_args()
+
+    print('Model: %s' % (args.model))
+    generate_final_predictions(args.model)
